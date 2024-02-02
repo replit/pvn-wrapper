d81aa2d729050aa1ddd1617fda668c8c7849dd6d
Anup Chenthamarakshan <anup@prodvana.io>
2024-02-02 12:49:10 -0800

    Print command before running it for easier debugging (#35)

--- cmdutil/cmd.go
+++ cmdutil/cmd.go
@@ -2,6 +2,7 @@ package cmdutil
 
 import (
 	go_errors "errors"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -12,18 +13,20 @@ import (
 func RunCmd(cmd *exec.Cmd) error {
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
+	log.Printf("Running command: %s", cmd.String())
 	if err := cmd.Run(); err != nil {
-		return errors.Wrapf(err, "Command failed:\n%s", strings.Join(cmd.Args, " "))
+		return errors.Wrapf(err, "Command failed:\n%s", cmd.String())
 	}
 	return nil
 }
 
 func RunCmdOutput(cmd *exec.Cmd) ([]byte, error) {
+	log.Printf("Running command: %s", cmd.String())
 	output, err := cmd.Output()
 	if err != nil {
 		var exitErr *exec.ExitError
 		if go_errors.As(err, &exitErr) {
-			return nil, errors.Wrapf(err, "Command failed:\n%s\n%s", strings.Join(cmd.Args, " "), string(exitErr.Stderr))
+			return nil, errors.Wrapf(err, "Command failed:\n%s\n%s", cmd.String(), string(exitErr.Stderr))
 		}
 		return nil, errors.Wrapf(err, "Command failed for unknown reasons:\n%s", strings.Join(cmd.Args, " "))
 	}
